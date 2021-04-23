ad6dcef4e354a7450fe5bc2d0eef5f1d086473fb
Samantha Humphrey <samahump546@outlook.com>
2021-04-23 09:36:01 -0400

    Add project files.

---
+++ PlayGameProject/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace PlayGameProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Bahnschrift Condensed", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(268, 42);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(272, 133);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Bahnschrift Condensed", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(268, 285);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(272, 44);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(782, 449);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cord Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ PlayGameProject/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;//allow us to use a sound player
+
+namespace PlayGameProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            //clear screen
+            playButton.Visible = false;
+
+            //show changes and pause
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change message in output label
+            outputLabel.Text = "Game Will Start In: 3";
+
+            //create sound player and create new sound
+            SoundPlayer readySetPlayer = new SoundPlayer(Properties.Resources.readySet);
+            readySetPlayer.Play();
+
+            //show changes and pause
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change message in output label
+            outputLabel.Text = "Game Will Start In: 2";
+
+            //show changes and pause
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change message in output label
+            outputLabel.Text = "Game Will Start In: 1";
+
+            //show changes and pause
+            Refresh();
+            Thread.Sleep(1000);
+
+            //change background colour of form
+            BackColor = Color.Lime;
+
+            //change message in output label
+            outputLabel.Text = "GO!";
+
+            //create sound player and create new sound
+            SoundPlayer goPlayer = new SoundPlayer(Properties.Resources.go);
+            goPlayer.Play();
+
+
+
+        }
+    }
+}
